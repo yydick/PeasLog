26d1e737da1ab5269b719f6a3d04cc2f3eafc43e
yydick <yydick@sohu.com>
2020-06-08 21:53:10 +0800

    SeasLog for PHP

---
+++ src/Exception/PedisLogException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Spool\PeasLog\Exceptions;
+
+use Exception;
+
+/**
+ * Description of PedisLogException
+ *
+ * @author 大天使长
+ */
+class PedisLogException extends Exception
+{
+    //put your code here
+}
